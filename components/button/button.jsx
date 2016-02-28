946c59bc7cb6c5ae155e7b144e801f3129ee54ea
Xingchen Hong <hello@xc-h.net>
2016-02-28 02:00:35 -0600

    Update button to 1.1.1.

--- components/button/button.jsx
+++ components/button/button.jsx
@@ -1,4 +1,26 @@
+/**
+ * @license
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 let { Component, PropTypes } = React;
+
+/**
+ * Implemented since 1.0.6.
+ * No changes from 1.0.6 to 1.1.1.
+ */
 
 class MaterialButton extends Component {
 
@@ -17,6 +39,8 @@ class MaterialButton extends Component {
     if (this.element_) {
       this.element_.removeEventListener('mouseup', this.boundButtonBlurHandler);
       this.element_.removeEventListener('mouseleave', this.boundButtonBlurHandler);
+
+      // The ripple will downgrade itself if unmounted.
     }
   }
 
@@ -74,6 +98,7 @@ class MaterialButton extends Component {
     }
     let classNameString = classNames.join(" ");
 
+    // Create ripple if requested.
     let rippleContainer = null;
     if (ripple) {
       rippleContainer = (
