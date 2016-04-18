Zodiase/meteor-mdl-react

commit 49b6c165371e9023edb0c24798db2fef90f43d71

Minor typo fix (I'm not sure what I was doing).

meteor-package/components/ripple/ripple.jsx
@@ -174,6 +174,7 @@ class MaterialRipple extends Component {
     this.state = this._getStateFromProps(props);
 
     this.element_ = null;
+    this.rippleElement_ = null;
 
     this.boundDownHandler = this.downHandler_.bind(this);
     this.boundUpHandler = this.upHandler_.bind(this);
@@ -195,7 +196,10 @@ class MaterialRipple extends Component {
   //componentDidUpdate(prevProps, prevState) {}
   render() {
     return (
-      <span className={this.state.className}>
+      <span
+        className={this.state.className}
+        ref={(ref) => this.element_ = ref}
+      >
         <span
           className={this.CssClasses_.RIPPLE}
           onMouseUp={this.props.onMouseUp}

commit 4df04d7fb6655b4b50a2705779ea9d629c08054f

Fix the error caused by 49b6c165371e9023edb0c24798db2fef90f43d71.

meteor-package/components/ripple/ripple.jsx
@@ -173,6 +173,7 @@ class MaterialRipple extends Component {
 
     this.state = this._getStateFromProps(props);
 
+    // `this.element_` should only be set by `bindElement`.
     this.element_ = null;
     this.rippleElement_ = null;
 
@@ -198,7 +199,6 @@ class MaterialRipple extends Component {
     return (
       <span
         className={this.state.className}
-        ref={(ref) => this.element_ = ref}
       >
         <span
           className={this.CssClasses_.RIPPLE}